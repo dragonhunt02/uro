9d003aeb06917dd088f5bf4ce1ad8d43eab7ad6b
dragonhunt02 <jackhunt43@libero.it>
2025-02-16 11:20:16 +0100

    Update application.ex

--- lib/uro/application.ex
+++ lib/uro/application.ex
@@ -1,3 +1,5 @@
+require Logger
+
 defmodule Uro.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
@@ -6,6 +8,7 @@ defmodule Uro.Application do
   use Application
 
   def start(_type, _args) do
+    validate_env()
     children =
       if System.get_env("MINIMAL_START") == "true",
         do: [],
@@ -22,6 +25,21 @@ defmodule Uro.Application do
     opts = [strategy: :one_for_one, name: Uro.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  defp validate_env do
+    env_vars = ["TURNSTILE_SECRET_KEY"]
+    for var <- env_vars do
+      err_msg = case var do
+        "TURNSTILE_SECRET_KEY" -> "Turnstile (a reCaptcha alternative) is disabled because the environment variable TURNSTILE_SECRET_KEY is not set. For more information, see https://developers.cloudflare.com/turnstile/get-started/."
+        _ -> "Environment variable #{var} is not set"
+      end
+
+      case System.get_env(var) do
+        nil -> Logger.warning(err_msg)
+        _ -> Logger.info("Environment variable #{var} is set")
+      end
+    end
+  end
 
   # Tell Phoenix to update the endpoint configuration
   # whenever the application is updated.
