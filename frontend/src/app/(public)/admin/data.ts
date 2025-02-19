230b3057942c8f195ac31fe86b78a8fec03433c5
dragonhunt02 <jackhunt43@libero.it>
2025-02-19 16:52:54 +0100

    Create data.ts

---
+++ frontend/src/app/(public)/admin/data.ts
@@ -0,0 +1,34 @@
+import { skipToken, useSuspenseQuery } from "@tanstack/react-query";
+
+import {
+	type api,
+	getAdminStatus,
+	type User
+} from "~/api";
+import { useOptionalSession } from "~/hooks/session";
+import { getQueryClient } from "~/query";
+
+export function useAdminStatus() {
+	const session = useOptionalSession();
+	// if (!session) return null;
+
+	const { data: admin_status = null } = useSuspenseQuery({
+		queryFn:
+			async ({ signal }) => {
+					const { data, error, response } = await getAdminStatus({
+						signal
+					});
+
+					if (response.status === 404) return null;
+					if (error) throw error;
+					return data;
+				},
+		queryKey: ["adminStatus"]
+	});
+
+	if (!admin_status) return null;
+
+	return {
+		...admin_status,
+	};
+}
