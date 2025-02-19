8f44396a9bc66dc3e41a856d76ea39c781b3edde
dragonhunt02 <jackhunt43@libero.it>
2025-02-19 15:27:46 +0100

    Create pages.tsx

---
+++ frontend/src/app/(public)/admin/pages.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { VSekaiMark } from "~/components/vsekai-mark";
+import { useReturnIntent } from "~/hooks/return-intent";
+import { restoreReturnIntent } from "~/hooks/return-intent/common";
+import { useSession } from "~/hooks/session";
+
+export default function AdminStatusPage() {
+	const session = useSession();
+	const { returnIntent } = useReturnIntent();
+
+	if (session.user.email_confirmed_at)
+		restoreReturnIntent(returnIntent?.href || "/");
+
+	return (
+		<div className="flex h-full grow items-center justify-center">
+			<div className="flex w-[32rem] flex-col gap-4 overflow-hidden rounded-xl border border-tertiary-300 bg-tertiary-50 p-8">
+				<span className="text-xl">
+					<VSekaiMark className="inline size-5" /> Admin Status
+				</span>
+				<p className="opacity-75">
+					We&apos;ve sent you an email to{" "}
+					<span className="font-medium">{session.user.email}</span> with a link
+					to confirm your account. Please check your inbox and click the link to
+					continue.
+				</p>
+				<div className="flex gap-2">
+				</div>
+			</div>
+		</div>
+	);
+}
