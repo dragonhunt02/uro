0b1f0c14aacc7ae21d7a1f81d2f74603904bc1ca
dragonhunt02 <jackhunt43@libero.it>
2025-02-21 12:23:20 +0100

    Update map.ex

--- lib/uro/controllers/map.ex
+++ lib/uro/controllers/map.ex
@@ -97,4 +97,45 @@ defmodule Uro.MapController do
         )
     end
   end
+
+  operation(:create,
+    operation_id: "createMap",
+    summary: "Create Map",
+    responses: [
+      ok: {
+        "",
+        "application/json",
+        %Schema{}
+      }
+    ]
+  )
+
+  def create(conn, %{"map" => map_params}) do
+    case UserContent.create_map(
+      Uro.Helpers.UserContentHelper.get_correct_user_content_params(conn, map_params, "user_content_data", "user_content_preview")) do
+      {:ok, map} ->
+        conn
+        |> put_status(200)
+        |> json(%{
+          data: %{
+            map:
+              Uro.Helpers.UserContentHelper.get_api_user_content(
+                map,
+                %{merge_uploader_id: true}
+              )
+          }
+        })
+
+      # Change prod to dev
+      {:error, %Ecto.Changeset{changes: changes, errors: errors} = changeset} ->
+        conn
+        |> put_status(500)
+        |> (fn conn ->
+          if Mix.env() == "prod" do
+            conn
+            |> json(%{changes: changes, errors: errors})
+          end
+        end).()
+    end
+  end
 end
