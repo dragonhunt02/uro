0a35c4f45d991a98d92a57e40cc21b75895f5cdc
dragonhunt02 <jackhunt43@libero.it>
2025-02-25 13:16:50 +0100

    Update avatar.ex

--- lib/uro/controllers/avatar.ex
+++ lib/uro/controllers/avatar.ex
@@ -33,6 +33,32 @@ defmodule Uro.AvatarController do
     })
   end
 
+  operation(:indexUploads,
+    operation_id: "listAvatarsUploads",
+    summary: "List Avatars uploaded by logged in user",
+    responses: [
+      ok: {
+        "",
+        "application/json",
+        %Schema{}
+      }
+    ]
+  )
+
+  def indexUploads(conn, _params) do
+    user = Uro.Helpers.Auth.get_current_user(conn)
+    avatars = UserContent.list_avatars_uploaded_by(user)
+
+    conn
+    |> put_status(200)
+    |> json(%{
+      data: %{
+        maps:
+          Uro.Helpers.UserContentHelper.get_api_user_content_list(avatars, %{
+            merge_uploader_id: true
+          })
+      }
+
   operation(:show,
     operation_id: "getAvatar",
     summary: "Get Avatar",
@@ -67,6 +93,157 @@ defmodule Uro.AvatarController do
           conn,
           400
         )
+    end
+  end
+
+  operation(:showUpload,
+    operation_id: "getAvatarUpload",
+    summary: "Get uploaded Avatar",
+    responses: [
+      ok: {
+        "",
+        "application/json",
+        %Schema{}
+      }
+    ]
+  )
+
+  def showUpload(conn, %{"id" => id}) do
+    user = Uro.Helpers.Auth.get_current_user(conn)
+
+    case UserContent.get_avatar_uploaded_by_user!(id, user) do
+      %Uro.UserContent.Avatar{} = avatar ->
+        conn
+        |> put_status(200)
+        |> json(%{
+          data: %{
+            avatar:
+              Uro.Helpers.UserContentHelper.get_api_user_content(
+                avatar,
+                %{merge_uploader_id: true, merge_is_public: true}
+              )
+          }
+        })
+
+      _ ->
+        put_status(
+          conn,
+          400
+        )
+    end
+  end
+
+  operation(:create,
+    operation_id: "createAvatar",
+    summary: "Create Avatar",
+    responses: [
+      ok: {
+        "",
+        "application/json",
+        %Schema{}
+      }
+    ]
+  )
+
+  def create(conn, %{"avatar" => avatar_params}) do
+    case UserContent.create_avatar(
+      Uro.Helpers.UserContentHelper.get_correct_user_content_params(conn, avatar_params, "user_content_data", "user_content_preview")) do
+      {:ok, avatar} ->
+        conn
+        |> put_status(200)
+        |> json(%{
+          data: %{
+            avatar:
+              Uro.Helpers.UserContentHelper.get_api_user_content(
+                avatar,
+                %{merge_uploader_id: true}
+              )
+          }
+        })
+
+      {:error, %Ecto.Changeset{changes: changes, errors: errors} = changeset} ->
+        conn
+        |> put_status(500)
+        |> (fn conn ->
+          if Mix.env() == "dev" do
+            conn
+            |> json(%{changes: changes, errors: errors})
+          end
+        end).()
+    end
+  end
+
+  operation(:update,
+    operation_id: "updateAvatar",
+    summary: "Update Avatar",
+    responses: [
+      ok: {
+        "",
+        "application/json",
+        %Schema{}
+      }
+    ]
+  )
+
+  def update(conn, %{"id" => id, "avatar" => avatar_params}) do
+    user = Uro.Helpers.Auth.get_current_user(conn)
+    avatar = UserContent.get_avatar_uploaded_by_user!(id, user)
+
+    case UserContent.update_avatar(avatar, avatar_params) do
+      {:ok, avatar} ->
+        conn
+        |> put_status(200)
+        |> json(%{
+          data: %{
+            avatar:
+              Uro.Helpers.UserContentHelper.get_api_user_content(
+                avatar,
+                %{merge_uploader_id: true}
+              )
+          }
+        })
+
+      {:error, %Ecto.Changeset{changes: changes, errors: errors} = changeset} ->
+        conn
+        |> put_status(500)
+        |> (fn conn ->
+          if Mix.env() == "dev" do
+            conn
+            |> json(%{changes: changes, errors: errors})
+          end
+        end).()
+    end
+  end
+
+  operation(:delete,
+    operation_id: "deleteAvatar",
+    summary: "Delete Avatar",
+    responses: [
+      ok: {
+        "",
+        "application/json",
+        %Schema{}
+      }
+    ]
+  )
+
+  def delete(conn, %{"id" => id}) do
+    user = Uro.Helpers.Auth.get_current_user(conn)
+
+    case UserContent.get_avatar_uploaded_by_user!(id, user) do
+      %Uro.UserContent.Avatar{} = avatar ->
+        case UserContent.delete_map(avatar) do
+          {:ok, _avatar} ->
+            conn
+            |> put_status(200)
+
+          {:error, %Ecto.Changeset{}} ->
+            conn
+            |> put_status(500)
+        end
+      _ ->
+        conn
+        |> put_status(200)
     end
   end
 end
