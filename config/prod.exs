793995331295770e22ba9cacb9253427d385ffcf
dragonhunt02 <jackhunt43@libero.it>
2025-03-05 03:56:39 +0100

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -3,5 +3,8 @@ import Config
 # Unused (no static files)
 # config :uro, Uro.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
+# TODO: Replace with correct adapter in production
+config :uro, Uro.Mailer, adapter: Swoosh.Adapters.Local
+
 # Do not print debug messages in production.
 config :logger, level: :info
