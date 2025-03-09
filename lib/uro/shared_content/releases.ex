860df7b565af5a0d6adebd56ae412f66f0859bef
dragonhunt02 <jackhunt43@libero.it>
2025-03-09 05:35:01 +0100

    Update releases.ex

--- lib/uro/shared_content/releases.ex
+++ lib/uro/shared_content/releases.ex
@@ -1,23 +1,23 @@
-defmodule Uro.SharedContent.Releases do
+defmodule Uro.SharedContent.SharedFile do
   import Ecto.Changeset
   use Uro.SharedContent.SharedContent
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
   @derive {Phoenix.Param, key: :id}
-  schema "releases" do
+  schema "shared_files" do
     shared_content_fields()
 
     timestamps()
   end
 
   @doc false
-  def changeset(releases, attrs) do
-    shared_content_changeset(content, attrs)
+  def changeset(shared_file, attrs) do
+    shared_content_changeset(shared_file, attrs)
   end
 
   @doc false
-  def upload_changeset(releases, attrs) do
-    shared_content_upload_changeset(content, attrs)
+  def upload_changeset(shared_file, attrs) do
+    shared_content_upload_changeset(shared_file, attrs)
   end
 end
