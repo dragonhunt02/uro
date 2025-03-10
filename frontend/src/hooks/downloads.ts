0f41321665f7014e4eba97da7700ffd1cbc9f625
dragonhunt02 <jackhunt43@libero.it>
2025-03-10 05:24:54 +0100

    Create downloads.ts

---
+++ frontend/src/hooks/downloads.ts
@@ -0,0 +1,53 @@
+"use client";
+
+import { useQueryClient, useQuery } from "@tanstack/react-query";
+import { redirect } from "next/dist/client/components/redirect";
+
+import { listSharedFiles } from "~/api";
+import { getQueryClient } from "~/query";
+
+import { useReturnIntent } from "./return-intent";
+import { useLocation } from "./location";
+
+export const useListSharedFiles = () => {
+  const { withReturnIntent } = useReturnIntent();
+  const { pathname } = useLocation();
+  const queryClient = useQueryClient();
+
+  const { data: sharedFiles } = useQuery({
+    queryFn: async () => {
+      const response = await listSharedFiles();
+      if (!response.ok) {
+        //throw new Error("Network response was not ok");
+        return null;
+      }
+      return response.json();
+    },
+    queryKey: ["shared-files"],
+    refetchOnWindowFocus: "always"
+  });
+
+  return sharedFiles;
+};
+
+export const useListSharedFiles = () => {
+	const { withReturnIntent } = useReturnIntent();
+	const { pathname } = useLocation();
+	const queryClient = useQueryClient();
+
+	const { data: session } = useSuspenseQuery({
+		queryFn: async () => {
+			const data = await getOptionalSession();
+			if (!data) return null;
+
+			queryClient.setQueryData(["users", data.user.username], data.user);
+			return data;
+		},
+		queryKey: ["session"],
+		refetchOnWindowFocus: "always"
+	});
+
+
+	return session;
+};
+
