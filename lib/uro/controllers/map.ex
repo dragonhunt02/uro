e67f8c903a2a03d64366f55e9cefbf1d55928af1
dragonhunt02 <jackhunt43@libero.it>
2025-03-11 02:49:01 +0100

    Update map.ex

--- lib/uro/controllers/map.ex
+++ lib/uro/controllers/map.ex
@@ -228,14 +228,15 @@ defmodule Uro.MapController do
           {:ok, _map} ->
             conn
             |> put_status(200)
+            |> json(%{ data: %{} })
 
-          {:error, %Ecto.Changeset{}} ->
-            conn
-            |> put_status(500)
+          {:error, %Ecto.Changeset{changes: _changes, errors: _errors} = changeset} ->
+            {:error, changeset}
         end
       _ ->
         conn
         |> put_status(200)
+        |> json(%{ data: %{} })
     end
   end
 end
