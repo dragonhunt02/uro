1a3e9ebafd77dd17f1ea4a334db4f57acbf455fc
dragonhunt02 <jackhunt43@libero.it>
2025-03-25 09:41:32 +0100

    Update 20250204102214_create_sharedfiles.exs

--- priv/repo/migrations/20250204102214_create_sharedfiles.exs
+++ priv/repo/migrations/20250204102214_create_sharedfiles.exs
@@ -7,18 +7,14 @@ defmodule Uro.Repo.Migrations.CreateSharedFiles do
       add :name, :string
       add :description, :string
       add :shared_content_data, :string
-      add :file_path, :string
       add :file_size, :integer
-      add :file_type, :string
       add :checksum, :string
-      add :upload_date, :utc_datetime
       add :uploader_id, references(:users, type: :uuid)
       add :version, :string
       add :tags, {:array, :string}
-      add :permissions, :map
       add :is_public, :boolean, default: false, null: false
 
-      timestamps()
+      timestamps(inserted_at: :created_at)
     end
   end
 end
