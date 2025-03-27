1200d88ffd5dc524409774e178ed0e615fbcaf9b
dragonhunt02 <jackhunt43@libero.it>
2025-03-27 09:15:50 +0100

    Update prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,6 @@ import Config
 # Unused (no static files)
 # config :uro, Uro.Endpoint, cache_static_manifest: "priv/static/cache_manifest.json"
 
-# 
 config :uro, Uro.Mailer,
   adapter: Swoosh.Adapters.Sendgrid,
   api_key: System.get_env("SENDGRID_API_KEY", "")
