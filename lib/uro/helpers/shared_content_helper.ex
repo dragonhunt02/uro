f32f57e913493bd4bb7a6074a7658cfdfa581ff2
dragonhunt02 <jackhunt43@libero.it>
2025-03-28 03:08:30 +0100

    Update shared_content_helper.ex

--- lib/uro/helpers/shared_content_helper.ex
+++ lib/uro/helpers/shared_content_helper.ex
@@ -9,7 +9,10 @@ defmodule Uro.Helpers.SharedContentHelper do
         to_string(
           Uro.Uploaders.SharedContentData.url({shared_content.shared_content_data, shared_content})
         ),
-      checksum: to_string(shared_content.checksum)
+      file_size: shared_content.file_size,
+      checksum: to_string(shared_content.checksum),
+      version: to_string(shared_content.version),
+      tags: shared_content.tags
     })
   end
 
