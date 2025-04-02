2da0dd198a2bb7f98248f0a63c2a16db8dc1769d
dragonhunt02 <jackhunt43@libero.it>
2025-04-03 01:10:51 +0200

    Update downloads-table.tsx

--- frontend/src/components/downloads-table.tsx
+++ frontend/src/components/downloads-table.tsx
@@ -55,7 +55,7 @@ export const DownloadsTable: React.FC<ComponentProps<"div"> & { data: SharedFile
                 <th className="px-4 py-2 border-r border-gray-200">Name</th>
                 <th className="px-4 py-2 border-r border-gray-200">Platform</th>
                 <th className="px-4 py-2 border-r border-gray-200">Size (MB)</th>
-                <th className="px-4 py-2">Checksum</th>
+                <th className="px-4 py-2">SHA256 Checksum</th>
               </tr>
             </thead>
             <tbody className="bg-white divide-y divide-gray-200">
