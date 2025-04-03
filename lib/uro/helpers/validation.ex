c7d2f693a0036b2ac49db8f69ea758cebe8220af
dragonhunt02 <jackhunt43@libero.it>
2025-04-03 22:25:22 +0200

    Update validation.ex

--- lib/uro/helpers/validation.ex
+++ lib/uro/helpers/validation.ex
@@ -19,14 +19,15 @@ def check_magic_exmarcel(%{file_name: file_name, path: path}) do
   ext_mime = ExMarcel.MimeType.for(nil, extension: file_extension)
   IO.puts(magic_mime)
   IO.puts(ext_mime)
-  if magic_mime.type == ext_mime.type do
-    true
-  elif magic_mime == "application/octet-stream" # exmarcel fallback value when not in magic list
-    Logger.warning("File magic number not recognized: #{file_extension} in #{file_name}. Skipping magic number validation...")
-    true
-  else
-    IO.puts("Wrong file")
-    false
+  cond do
+    magic_mime.type == ext_mime.type ->
+      true
+    magic_mime == "application/octet-stream" -> # exmarcel fallback value when not in magic list
+      Logger.warning("File magic number not recognized: #{file_extension} in #{file_name}. Skipping magic number validation...")
+      true
+    _ ->
+      IO.puts("Wrong file")
+      false
   end
 end
 
