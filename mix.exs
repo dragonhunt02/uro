f4aa24180898e3c05541c1d2b21a1c0c38338a1b
dragonhunt02 <jackhunt43@libero.it>
2025-04-03 19:47:07 +0200

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -75,7 +75,8 @@ defmodule Uro.MixProject do
       {:ecto_commons, "~> 0.3.4"},
       {:swoosh, "~> 1.3"},
       {:hammer, "~> 6.0"},
-      {:scrivener_ecto, "~> 2.7"}
+      {:scrivener_ecto, "~> 2.7"},
+      {:ex_marcel, "~> 0.1.0"}
     ]
   end
 
