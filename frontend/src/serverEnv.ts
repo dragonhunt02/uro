c5ec01bb5a0b40fa62360f7b5e7345f167dec6c6
dragonhunt02 <jackhunt43@libero.it>
2025-04-06 20:59:38 +0200

    Update serverEnv.ts

--- frontend/src/serverEnv.ts
+++ frontend/src/serverEnv.ts
@@ -3,3 +3,11 @@ export async function prefetchEnvVariables() {
   const data = await response.json();
   localStorage.setItem('envCache', JSON.stringify(data));
 }
+
+export function getEnvVariables(): { apiOrigin: string } {
+  const cachedData = localStorage.getItem('envCache');
+  if (!cachedData) {
+    throw new Error('Environment variables are not available yet!');
+  }
+  return JSON.parse(cachedData);
+}
