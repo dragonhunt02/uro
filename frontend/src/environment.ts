67c09ab6a71c0f2e50b0ff0109b89b9dfe702ef9
dragonhunt02 <jackhunt43@libero.it>
2025-04-06 18:59:23 +0200

    Create environment.ts

---
+++ frontend/src/environment.ts
@@ -0,0 +1,60 @@
+interface Env {
+  origin?: string;
+  apiOrigin?: string;
+  turnstileSiteKey?: string;
+}
+
+let cachedEnv: Env = {};
+
+//let cachedEnv: Record<string, any> | null = null;
+
+function initializeEnvironment() {
+  if (!cachedEnv) {
+    const localStorageEnv = localStorage.getItem('env');
+    if (localStorageEnv) {
+      cachedEnv = JSON.parse(localStorageEnv);
+    } else {
+      const xhr = new XMLHttpRequest();
+      xhr.open('GET', '/api/env', false); // Synchronous XMLHttpRequest
+      xhr.send();
+
+      if (xhr.status === 200) {
+        cachedEnv = JSON.parse(xhr.responseText);
+        localStorage.setItem('env', JSON.stringify(cachedEnv));
+      } else {
+        throw new Error('Failed to fetch environment variables');
+      }
+    }
+  }
+}
+
+// Initialize environment variables on module load
+initializeEnvironment();
+
+export const origin = cachedEnv?.origin ?? '';
+export const apiOrigin = cachedEnv?.apiOrigin ?? '';
+export const turnstileSiteKey = cachedEnv?.turnstileSiteKey ?? '';
+//export const urls = cachedEnv?.urls || {};
+
+/**
+ * A set of first-party origins, these are given special treatment in the
+ * application, such as in OAuth2 redirection & opening links in a new tab.
+ */
+export const firstPartyOrigins = new Set([cachedEnv?.origin ?? '']);
+
+//function environment<T>(value: unknown, name: string): T {
+//	if (!value) throw new Error(`Missing environment variable: ${name}.`);
+//	return value as T;
+//}
+
+export const development = process.env.NODE_ENV === "development";
+
+if (development) {
+	process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
+}
+
+export const urls = {
+	discord: "https://discord.gg/7BQDHesck8",
+	github: "https://github.com/v-sekai",
+	twitter: "https://twitter.com/vsekaiofficial"
+};
