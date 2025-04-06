18220cccdfb7998ca3199af662ab3efd85f2e95c
dragonhunt02 <jackhunt43@libero.it>
2025-04-06 22:02:27 +0200

    Update ClientPrefetch.tsx

--- frontend/src/components/ClientPrefetch.tsx
+++ frontend/src/components/ClientPrefetch.tsx
@@ -5,9 +5,13 @@ import { prefetchEnvVariables } from '~/serverEnv';
 
 export function ClientPrefetch() {
   useEffect(() => {
+    if (typeof window !== 'undefined') {
     prefetchEnvVariables().catch((error) =>
       console.error('Error prefetching environment variables:', error)
     );
+    } else {
+      console.log('Cant prefetch env server sideError prefetching environment variables:')
+    }
   }, []);
 
   return null; // This component doesn't render anything visible
