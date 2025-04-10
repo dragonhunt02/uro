b6ac403f8e541d5b9923be4fbd31bff6ebde4fda
dragonhunt02 <jackhunt43@libero.it>
2025-04-11 00:21:01 +0200

    Update environment.ts

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -5,21 +5,6 @@ function environment<T>(value: unknown, name: string): T {
 
 export const development = process.env.NODE_ENV === "development";
 
-export const origin = environment<string>(
-	process.env.NEXT_PUBLIC_ORIGIN,
-	"NEXT_PUBLIC_ORIGIN"
-);
-
-export const apiOrigin = environment<string>(
-	process.env.API_ORIGIN || process.env.NEXT_PUBLIC_API_ORIGIN,
-	"API_ORIGIN and or NEXT_PUBLIC_API_ORIGIN"
-);
-
-export const turnstileSiteKey = environment<string>(
-	process.env.NEXT_PUBLIC_TURNSTILE_SITEKEY,
-	"NEXT_PUBLIC_TURNSTILE_SITEKEY"
-);
-
 if (development) {
 	process.env["NODE_TLS_REJECT_UNAUTHORIZED"] = "0";
 }
@@ -29,13 +14,6 @@ export const urls = {
 	github: "https://github.com/v-sekai",
 	twitter: "https://twitter.com/vsekaiofficial"
 };
-
-/**
- * A set of first-party origins, these are given special treatment in the
- * application, such as in OAuth2 redirection & opening links in a new tab.
- */
-export const firstPartyOrigins = new Set([origin]);
-
 
 // Runtime fetch from API
 interface serverEnvType {
@@ -100,12 +78,12 @@ export const getServerEnv = (): serverEnvType | null => {
  * application, such as in OAuth2 redirection & opening links in a new tab.
  * Add to set below if required.
  */
-const firstPartyOrigins_new: Set<string> = new Set([]);
+const firstPartyOrigins: Set<string> = new Set([]);
 
 export const getFirstPartyOrigins = (): Set<string> => {
   const serverEnv = getServerEnv();
   const appOrigin = serverEnv?.origin;
-  let origins = firstPartyOrigins_new;
+  let origins = firstPartyOrigins;
 
   // Ensure firstPartyOrigins includes the fetched origin
   if (appOrigin) {
