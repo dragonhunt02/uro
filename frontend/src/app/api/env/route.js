e78f46fe7a34a322c59f591cfb913734a3048bdc
dragonhunt02 <jackhunt43@libero.it>
2025-04-10 22:01:07 +0200

    Update route.js

--- frontend/src/app/api/env/route.js
+++ frontend/src/app/api/env/route.js
@@ -4,7 +4,7 @@ export const dynamic = 'force-dynamic';
 export async function GET(request) {
   const responseBody = {
     origin: process.env.NEXTJS_ORIGIN,
-    apiOrigin: process.env.API_ORIGIN || process.env.NEXT_PUBLIC_API_ORIGIN,
+    apiOrigin: process.env.NEXTJS_API_ORIGIN,
     turnstileSiteKey: process.env.NEXTJS_TURNSTILE_SITEKEY,
   };
 
