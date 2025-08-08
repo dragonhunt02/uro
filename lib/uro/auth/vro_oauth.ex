3a7c507a646e0ac139a0618cd651a0f2a2432cfb
dragonhunt02 <jackhunt43@libero.it>
2025-08-08 02:36:09 +0200

    Update vro_oauth.ex

--- lib/uro/auth/vro_oauth.ex
+++ lib/uro/auth/vro_oauth.ex
@@ -3,18 +3,22 @@ defmodule Uro.Oauth.Vroid do
 
   @impl true
   def default_config(_config) do
+     config = Application.get_env(:uro, :pow_assent)[:providers][:vroid]
     [
       base_url: "https://hub.vroid.com",
       authorize_url: "/oauth/authorize",
       token_url: "/oauth/token",
       user_url: "/api/account",
 
-      authorization_params: [response_type: "code", scope: "default"],
+      authorization_params: [response_type: "code", scope: "default"],  #, client_id: config[:client_id], client_secret: config[:client_secret]],
       token_params: [grant_type: "authorization_code"],
+token_request_method: :get,
 
-      headers: [{"X-Api-Version", "11"}],
+      #headers: [{"X-Api-Version", "11"}],
+      #auth_headers: [{"X-Api-Version", "10"}],
+      base_headers: [{"X-Api-Version", "11"}],
 
-      auth_method: :client_secret_basic
+      auth_method: :client_secret_post
     ]
   end
 
@@ -27,12 +31,14 @@ defmodule Uro.Oauth.Vroid do
         }
       }) do
     avatar = get_in(user, ["icon", "sq170", "url"])
+    IO.inspect(avatar)
 
     {:ok,
      %{
        "sub"     => user["id"],
        "name"    => user["name"],
-       "picture" => avatar
+       "picture" => avatar,
+       "email" => "#{user["id"]}@vroid.vsekai.local" # placeholder
     }}
   end
 end
